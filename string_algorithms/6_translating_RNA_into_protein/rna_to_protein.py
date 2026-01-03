aef809ef06a0f4f7c82965f48a866dbd57bbdcb9
jannettas <zsoltani@student.42lausanne.ch>
2026-01-03 20:38:30 +0100

    rna to protein

---
+++ string_algorithms/6_translating_RNA_into_protein/rna_to_protein.py
@@ -0,0 +1,30 @@
+RNA_CODON_TABLE = {
+    'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
+    'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V',
+    'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V',
+    'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
+    'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
+    'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+    'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+    'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+    'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+    'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+    'UAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+    'UAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+    'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+    'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+    'UGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+    'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'
+}
+
+with open("rosalind_prot.txt", "r") as f:
+	rna_sequence = f.read().strip()
+
+protein_sequence = []
+for i in range(0, len(rna_sequence), 3):
+	codon = rna_sequence[i:i+3]
+	amino_acid = RNA_CODON_TABLE.get(codon, '')
+	if amino_acid == 'Stop':
+		break
+	protein_sequence.append(amino_acid)
+print("".join(protein_sequence))
